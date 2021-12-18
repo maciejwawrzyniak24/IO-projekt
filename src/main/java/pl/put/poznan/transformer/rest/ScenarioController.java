6aab40fa941642bfd9276b299903cb82b2e440b2
maciejwawrzyniak24 <92788521+maciejwawrzyniak24@users.noreply.github.com>
2021-12-18 11:57:28 +0100

    Usuniecie przykladowego wizytatora

--- src/main/java/pl/put/poznan/transformer/rest/ScenarioController.java
+++ src/main/java/pl/put/poznan/transformer/rest/ScenarioController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import pl.put.poznan.transformer.logic.Scenario;
-import pl.put.poznan.transformer.visitor.ExampleVisitor;
 import pl.put.poznan.transformer.visitor.HowManyStepsVisitor;
 import pl.put.poznan.transformer.visitor.StepsWithKeywordsVisitor;
 import pl.put.poznan.transformer.visitor.StepsWithoutAuthorVisitor;
@@ -19,31 +18,34 @@ import pl.put.poznan.transformer.visitor.StepsWithoutAuthorVisitor;
 public class ScenarioController {
 
     /**
-     * Funkcja umozliwia odebranie z API scenariusza w formacie json
+     * Funkcja umozliwia policzenie liczby kroków w scenariuszu
      */
-    @RequestMapping(method = RequestMethod.POST, value = "/example-visitor")
-    public Object setScenario(@RequestBody Scenario scenario) {
-        var visitor = new ExampleVisitor();
-        scenario.accept(visitor);
-        return visitor.getResult();
-    }
-
     @RequestMapping(method = RequestMethod.POST, value = "/example-howmanystepsvisitor")
-    public Object setScenario2(@RequestBody Scenario scenario) {
+    public Object setScenario1(@RequestBody Scenario scenario) {
         var visitor = new HowManyStepsVisitor();
         scenario.accept(visitor);
         return visitor.getResult();
     }
 
+    /**
+     * Funkcja umożliwia policzenie krokow w scenariuszu, zaczynajacych sie od slow kluczowych
+     * @param scenario
+     * @return
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/stepswithkeywords-visitor")
-    public Object setScenario3(@RequestBody Scenario scenario) {
+    public Object setScenario2(@RequestBody Scenario scenario) {
         var visitor = new StepsWithKeywordsVisitor();
         scenario.accept(visitor);
         return visitor.getResult();
     }
 
+    /**
+     * Funkcja umożliwia sprawdzenie czy w danym scenariuszu są kroki nie zaczynajace sie od slow kluczowych ani aktorow
+     * @param scenario
+     * @return
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/stepswithoutauthor-visitor")
-    public Object setScenario4(@RequestBody Scenario scenario) {
+    public Object setScenario3(@RequestBody Scenario scenario) {
         var visitor = new StepsWithoutAuthorVisitor();
         scenario.accept(visitor);
         return visitor.getResult();
